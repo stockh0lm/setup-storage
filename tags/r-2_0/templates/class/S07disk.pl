6b4a9b63518408b3e8878ec741adaf84c7e086fc
(no author) <(no author)@ba5ec265-b0fb-0310-8e1a-cf9e4c2b1591>
2001-05-15 12:29:08 +0000

    This commit was manufactured by cvs2svn to create tag 'r-2_0'.
    git-svn-id: svn+ssh://svn.debian.org/svn/fai@642 ba5ec265-b0fb-0310-8e1a-cf9e4c2b1591

---
+++ tags/r-2_0/templates/class/S07disk.pl
@@ -0,0 +1,30 @@
+#! /usr/bin/perl
+
+# define classes for different disk configurations
+
+# global variables:
+# $numdisks             # number of disks
+# %disksize{$device}    # size for each device in MB
+# $sum_disk_size        # sum of all disksizes in MB
+
+use Debian::Fai;
+
+read_disk_info();
+
+# rules for classes
+#-------------------------------------------------------
+# two SCSI disks 2-5 GB
+($numdisks == 2) and
+    disksize(sda,2000,5400) and
+    disksize(sdb,2000,5400) and
+    class("SD_2_5GB");
+
+# one disk 3.5-4.4 GB, IDE or SCSI
+($numdisks == 1) and
+    testsize($sum_disk_size,3500,4400) and
+    class("4GB");
+
+#-------------------------------------------------------
+# do not edit beyond this line
+
+exit;
