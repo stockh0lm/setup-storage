95ed0d60dac01dd62405bb4a07aeeff49d9f957d
(no author) <(no author)@ba5ec265-b0fb-0310-8e1a-cf9e4c2b1591>
2001-10-10 09:18:34 +0000

    This commit was manufactured by cvs2svn to create tag 'r-2_2_2'.
    git-svn-id: svn+ssh://svn.debian.org/svn/fai@862 ba5ec265-b0fb-0310-8e1a-cf9e4c2b1591

---
+++ tags/r-2_2_2/templates/class/S90partitions.pl
@@ -0,0 +1,28 @@
+#! /usr/bin/perl
+
+# read one disk config file and define classes depending on partition names
+# (c) Thomas Lange, 2001, lange@informatik.uni-koeln.de
+
+sub match {
+    # here you can add more definitions
+    m#\s/scratch\s#          && print "NFS_SERVER SCRATCH ";
+    m#\s/files/scratch\s#    && print "NFS_SERVER FILES_SCRATCH ";
+    m#\s/tmp\s#              && print "TMP_PARTITION ";
+    m#\s/fai-boot\s#         && print "FAI_BOOTPART ";
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# main routine is read only for you
+foreach $class ( $ENV{HOSTNAME}, split /\s+/, $ENV{classes}) {
+  $file = "/fai/disk_config/$class";
+  next unless -f $file;
+  open (PART,"<$file") || die "Can't open $file\n";
+  while (<PART>) {
+    # skip comments
+    next if /^#/;
+    &match;
+  }
+  close PART;
+  # read only one config file
+  exit 0;
+}
